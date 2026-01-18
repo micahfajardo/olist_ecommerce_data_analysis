f515bff9390b1a011f3c689bd60a6520d0e8c39f
Micah Theresse Fajardo <micahtheresse@gmail.com>
2026-01-19 06:29:19 +0800

    logistics_efficiency.sql

--- sql_queries/logistics_efficiency.sql
+++ sql_queries/logistics_efficiency.sql
@@ -1,11 +1,11 @@
 
-/* Logistics Efficiency Analytics: contain queries used to report the logistics efficiency of the orders 
-and report peak hours and peak days to be used to improve logistics efficiency
+/* Logistics Efficiency Analytics: contain queries used to report the logistics efficiency of the orders.
 */
 
 -- Query that list how many days it take from order of purchase to delivery day
 CREATE OR REPLACE VIEW logistics_efficiency AS
 SELECT 
+	DATE (o.order_purchase_timestamp) AS order_day,
     o.order_id,
     o.customer_id,
     o.order_status,
@@ -26,21 +26,10 @@ SELECT
 FROM order_info o
 -- Subquery to join rows of multiple items from one order
 LEFT JOIN (
-    SELECT order_id, MAX(shipping_limit_date) as shipping_limit_date
+    SELECT 
+    order_id, 
+    MAX(shipping_limit_date) as shipping_limit_date
     FROM order_items
     GROUP BY order_id
 ) i ON o.order_id = i.order_id
 WHERE order_status = 'delivered';
-
-CREATE OR REPLACE VIEW peak_seasons AS
--- Query for peak hours and days
-SELECT 
-    -- extract hour when order is made
-    HOUR(order_purchase_timestamp) AS purchase_hour,
-      -- extract day of the week when order is made
-    DAYNAME(order_purchase_timestamp) AS purchase_day,
-    COUNT(o.order_id) AS total_orders
-FROM order_info o
-LEFT JOIN order_payments p ON o.order_id = p.order_id
-GROUP BY purchase_hour, purchase_day
-ORDER BY purchase_hour;
